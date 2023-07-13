b4166be1e18e52ca6fc2b54c105cf3f3c82aad20
jorgepalaciosc <jorgepalaciios91@gmail.com>
2023-07-13 14:21:04 -0400

    adding nextjs

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,14 @@
+
+export default function AboutMe() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-6xl items-center justify-between font-mono text-lg lg:flex">
+        <h1 className="justify-center">
+          About me &nbsp;
+        </h1>
+      </div>
+
+
+    </main>
+  )
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Projects() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-6xl items-center justify-between font-mono text-lg lg:flex">
+        <h1 className="justify-center">
+          My Projects &nbsp;
+        </h1>
+      </div>
+
+
+    </main>
+  )
+}
