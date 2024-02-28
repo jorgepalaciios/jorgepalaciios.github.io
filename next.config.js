edbf6ba267718e0d127d1feea8dc573fb4b645af
jorgepalaciios <jorgepalaciios91@gmail.com>
2024-02-28 14:28:14 -0400

    updates for github actions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/2048-in-react",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-};
+  output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './my-loader.ts',
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
