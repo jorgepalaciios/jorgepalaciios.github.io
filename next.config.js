ffe61d9eb5421bc7a221a16f5780a7888616f064
jorgepalaciios <jorgepalaciios91@gmail.com>
2024-02-28 13:20:56 -0400

    github pages config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "/2048-in-react",
-  // output: "export",  // <=== enables static exports
-  // reactStrictMode: true,
+  basePath: "/2048-in-react",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
