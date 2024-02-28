59f7272adef7ce1c2aa7ee4f711754ee029762c7
Jorge Palacios <jorgepalaciios91@gmail.com>
2024-02-28 13:24:20 -0400

    Update next.config.js 2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/2048-in-react",
-  output: "export",  // <=== enables static exports
+  output: "export",
   reactStrictMode: true,
 };
 
