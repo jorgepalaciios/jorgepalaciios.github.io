39e409e45a61a852817ba28c7dcf5a440a37d951
jorgepalaciios <jorgepalaciios91@gmail.com>
2024-02-28 14:52:30 -0400

    basePath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/2048-in-react",
   output: 'export',
   images: {
     loader: 'custom',
     loaderFile: './my-loader.ts',
   },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
