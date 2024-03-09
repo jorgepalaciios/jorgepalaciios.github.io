7374bf3c4ab74050e8f61ff83489d4a53b98166c
jorgepalaciios <jorgepalaciios91@gmail.com>
2024-03-09 18:53:01 -0400

    style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@
 export default function Home() {
   return (
     <main className=" bg-[#6e6e6e]">
-      <section className='relative z-10 grid grid-cols-[1fr,min(640px,100%),1fr] gap-y-8 px-4 pt-48 font-sans text-base text-rose-100/90 xl:grid-cols-[1fr,minmax(auto,240px),min(640px,100%),minmax(auto,240px),1fr] xl:gap-x-9 xl:px-0 [&>*]:col-start-2 xl:[&>*]:col-start-3'>
+      <section className='relative z-10 grid grid-cols-[1fr,min(640px,100%),1fr] gap-y-8 px-4 pt-48 text-base xl:grid-cols-[1fr,minmax(auto,240px),min(640px,100%),minmax(auto,240px),1fr] xl:gap-x-9 xl:px-0 [&>*]:col-start-2 xl:[&>*]:col-start-3'>
         <h1>Hi my name is Jorge Palacios</h1>
       </section>
       {/* <div className="flex rounded-md bg-[#242424] p-4 ">
